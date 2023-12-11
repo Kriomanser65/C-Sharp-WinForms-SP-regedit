7bb8ad53d273d33464817e5b9e4373e7528e607c
Stekoks <Stekoks@DESKTOP-4SK39GD>
2023-12-11 10:55:31 +0200

    Add project files.

---
+++ Reg/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Reg
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 12);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(776, 233);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 251);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(776, 187);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ Reg/Form1.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Reg
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            LoadKeys(Registry.CurrentUser);
+            LoadKeys(Registry.LocalMachine);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void LoadKeys(RegistryKey rootKey)
+        {
+            await Task.Run(() =>
+            {
+                treeView1.Nodes.Add(new TreeNode(rootKey.Name));
+                AddSubKeys(rootKey, treeView1.Nodes[treeView1.Nodes.Count - 1]);
+            });
+        }
+
+        private void AddSubKeys(RegistryKey key, TreeNode node)
+        {
+            string[] subKeyNames = key.GetSubKeyNames();
+            foreach (string subKeyName in subKeyNames)
+            {
+                RegistryKey subKey = key.OpenSubKey(subKeyName);
+                if (subKey != null)
+                {
+                    TreeNode subNode = node.Nodes.Add(subKeyName);
+
+                    if (subNode.Level < 2)
+                        AddSubKeys(subKey, subNode);
+                }
+            }
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            listView1.Items.Clear();
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(e.Node.FullPath);
+
+            if (key != null)
+            {
+                foreach (string valueName in key.GetValueNames())
+                {
+                    object valueData = key.GetValue(valueName);
+                    ListViewItem item = listView1.Items.Add(valueName);
+                    item.SubItems.Add(valueData?.ToString());
+                }
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
